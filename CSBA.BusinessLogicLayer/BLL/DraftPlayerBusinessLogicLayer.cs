b4e412c9f38f0ce11ad1b04263ba99d92d9a10e5
CISC475 <bgibbons@ude.edu>
2014-04-21 14:22:59 -0400

    Latest Change

---
+++ CSBA.BusinessLogicLayer/BLL/DraftPlayerBusinessLogicLayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSBA.Contracts;
+using CSBA.DomainModels;
+using CSBA.DataAccessLayer;
+
+namespace CSBA.BusinessLogicLayer
+{
+    public class DraftPlayerBusinessLogicLayer
+    {
+        DraftPlayerDAL DAL = new DraftPlayerDAL();
+        public List<sp_SeasonTeamDraft_Select_ResultDomainModel> DraftTeamList(int SeasonID)
+        {
+            return DAL.DraftTeamList(SeasonID);
+        }
+
+        public PickAPlayerDomainModel PickAPLayer(int SeasonID)
+        {
+            return DAL.PickAPLayer(SeasonID);
+        }
+
+        public void DraftPlayer(SeasonTeamPlayerDomainModel STP)
+        {
+            DAL.DraftPlayer(STP);
+        }
+    }
+}
